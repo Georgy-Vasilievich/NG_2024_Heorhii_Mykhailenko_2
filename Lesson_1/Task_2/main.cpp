701886187dc3bd575084fedafa49f0a0d64ad861
Heorhii Mykhailenko <georgy_vasilievich@ukr.net>
2024-07-01 21:55:07 +0300

    Add Lesson 1 Task 2

---
+++ Lesson_1/Task_2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int firstNumber = 0;
+    int secondNumber = 0;
+
+    cout << "Enter the first number: ";
+    cin >> firstNumber;
+
+    cout << "Enter the second number: ";
+    cin >> secondNumber;
+
+    cout << firstNumber + secondNumber << endl;
+
+    return 0;
+}
