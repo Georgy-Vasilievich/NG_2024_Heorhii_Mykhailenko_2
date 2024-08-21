18759bba3cc45d293c5e686c0a6d6761009c188b
Heorhii Mykhailenko <georgy_vasilievich@ukr.net>
2024-08-21 14:06:43 +0300

    Add Lesson 1 Task 7

---
+++ Lesson_1/Task_7/main.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x = 0;
+    float y = 0;
+    float N = 0;
+    float distance = 0;
+
+    cout << "Enter your x: ";
+    cin >> x;
+
+    cout << "Enter your y: ";
+    cin >> y;
+
+    cout << "Enter radius N: ";
+    cin >> N;
+
+    distance = sqrt(pow(x, 2) + pow(y, 2));
+
+    cout << distance << " from the center";
+    if (distance > N)
+        cout << " (beyond radius)";
+    cout << endl;
+
+    return 0;
+}
